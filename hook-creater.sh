2da00132e06a2061b9907a2f3e44ef1851df76b4
Kento Tsuji <tuziemon@hotmail.co.jp>
2022-12-28 14:37:25 +0900

    Chore: quote variables
    follow strict POSIX style

--- hook-creater.sh
+++ hook-creater.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 query_installed_file_list() {
-  for e in $(pacman -Qlq $1 | xargs)
+  for e in $(pacman -Qlq "$1" | xargs)
   do
     if test "${e%/}" = "$e"; then
       files="${files} ${e}"
     fi
   done
 
-  echo $files
+  echo "$files"
 }
