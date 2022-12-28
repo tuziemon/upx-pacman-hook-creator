c429b1611629e9e95ffc42a4c626e28c13942565
highpon <s.shiraki.business@gmail.com>
2022-12-28 14:26:38 +0900

    Feature: Check file size

--- hook-creater.sh
+++ hook-creater.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+check_filesize() {
+    du_info=$(du -sh $1)
+    file_size="${du_info%%[[:space:]]*}"
+
+    if test "${file_size#*M}" != "$file_size"; then
+        echo 0
+    else
+        echo 1
+    fi
+}
